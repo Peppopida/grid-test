e4c21c6de683ab41be47cff200c927f718b9e3ba
Giuseppe Procida <procida@komodosoft.it>
2020-12-17 18:00:45 +0100

    test 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ElementBeach } from './model/element-beach.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'grid-test';
+export class AppComponent implements OnInit {
+  elementsBeach:ElementBeach[] = [
+    {id:10, name:'Cabina', status:-1, type:1, section:0, isEmpty:false},
+    {section:0, isEmpty:true},
+    {id:13, name:'Cabina', status:0, type:1, section:0, isEmpty:false},
+    {id:2, name:'Ombrellone', status:1, type:2, section:1, isEmpty:false},
+    {id:4, name:'Ombrellone', status:2, type:2, section:2, isEmpty:false},
+    {section:1, isEmpty:true},
+    {id:50, name:'Cabina', status:2, type:1, section:2, isEmpty:false},
+    {id:62, name:'Sdraio', status:2, type:3, section:4, isEmpty:false},
+    {section:2, isEmpty:true},
+    {id:624, name:'Lettone', status:0, type:4, section:2, isEmpty:false}
+  ];
+  statusName='';
+  isEmpty:boolean;
+
+  ngOnInit() {
+  }
+
+  getColor(status:number) {
+    switch(status) {
+      case -1:
+        this.statusName = 'ERRORE';
+        return '#C2C2C2';
+      case 0:
+        this.statusName = 'LIBERA';
+        return '#AAEC87';
+      case 1:
+        this.statusName = 'OCCUPATA';
+        return '#EC8787';
+      case 2:
+        this.statusName = 'PRENOTATA';
+        return '#87BAEC';
+    }
+  }
+
+  onSelectElementBeach(id:number) {
+    console.log(`Hai cliccato sull'elemento con id: ${id}`);
+  }
+
+   /**
+   * Dettagli singoli elementi della spiaggia:
+   * 
+   * id:
+   * 
+   * name:
+   * 
+   * status:  -1 error              #C2C2C2
+   *          0 free                #AAEC87
+   *          1 busy                #EC8787
+   *          2 reserved            #87BAEC
+   * 
+   * type:   -1 error
+   *          0 ombrellone
+   *          1 cabina
+   *          2 spogliatoio
+   *          3 capannina
+   *          4 lettone
+   *          5 pedalò
+   *          6 bananone
+   *          7 sdraio
+   *          8 sedia
+   *          9 moto d'acqua
+   *          10 canoa
+   *          11 servizio generico
+   * 
+   * section:
+   * 
+   * coords:  x (coordinata della colonna)
+   *          y (coordinata della riga)
+   */
 }
---
+++ src/app/model/element-beach.model.ts
@@ -0,0 +1,8 @@
+export class ElementBeach {
+  id?:number;
+  name?:string;
+  status?:number;
+  type?:number;
+  section:number;
+  isEmpty:boolean;
+}
