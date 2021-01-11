489b09f1e324e1af1f43688092780ec282f7c5ae
Giuseppe Procida <procida@komodosoft.it>
2021-01-11 10:28:10 +0100

    select only free element beach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,10 +22,10 @@ export class AppComponent implements OnInit {
   ];
   elementCoord:ElementCoord[] = [
     {id: 56, isFree: true, section: 0, coordX: 0, coordY: 0},
-    {id: 4, isFree: false, section: 0, coordX: 80, coordY: 80},
-    {id: 17, isFree: true, section: 0, coordX: 160, coordY: 160},
-    {id: 90, isFree: false, section: 0, coordX: 0, coordY: 240},
-    {id: 89, isFree: true, section:0, coordX: 80, coordY: 0}
+    {id: 4, isFree: false, section: 0, coordX: 40, coordY: 40},
+    {id: 17, isFree: true, section: 0, coordX: 80, coordY: 80},
+    {id: 90, isFree: false, section: 0, coordX: 0, coordY: 80},
+    {id: 89, isFree: true, section:0, coordX: 40, coordY: 0}
   ];
   pathElementFree = "./assets/icon/element-beach-free.png";
   pathElementBusy = "./assets/icon/element-beach-busy.png";
@@ -36,60 +36,13 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {}
 
-  getColor(status:number) {
-    switch(status) {
-      case -1:
-        this.statusName = 'ERRORE';
-        return '#C2C2C2';
-      case 0:
-        this.statusName = 'LIBERA';
-        return '#AAEC87';
-      case 1:
-        this.statusName = 'OCCUPATA';
-        return '#EC8787';
-      case 2:
-        this.statusName = 'PRENOTATA';
-        return '#87BAEC';
-    }
-  }
-
   getIcon(status:number, type:number):string {
     return this.iconService.getIcon(status, type);
   }
 
-  onSelectElementBeach(id:number) {
-    console.log(`Hai cliccato sull'elemento con id: ${id}`);
+  onSelectElementBeach(id:number, isFree:boolean) {
+    if(isFree){
+      console.log(`Hai cliccato sull'elemento con id: ${id}`);
+    }
   }
-
-   /**
-   * Dettagli singoli elementi della spiaggia:
-   * 
-   * id:
-   * 
-   * name:
-   * 
-   * status:  -1 error              #C2C2C2
-   *          0 free                #AAEC87
-   *          1 busy                #EC8787
-   *          2 reserved            #87BAEC
-   * 
-   * type:   -1 error
-   *          0 ombrellone
-   *          1 cabina
-   *          2 spogliatoio
-   *          3 capannina
-   *          4 lettone
-   *          5 pedalò
-   *          6 bananone
-   *          7 sdraio
-   *          8 sedia
-   *          9 moto d'acqua
-   *          10 canoa
-   *          11 servizio generico
-   * 
-   * section:
-   * 
-   * coords:  x (coordinata della colonna)
-   *          y (coordinata della riga)
-   */
 }
